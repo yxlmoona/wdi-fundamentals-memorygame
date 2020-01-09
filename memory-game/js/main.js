8abde31b9f8f4e45659b6b10140c1274eb7e4b7d
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-01-09 12:44:57 -0600

    create game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,16 +1,17 @@
-console.log("Up and running!");
+
 const cards =["queen", 'queen', "king", "king"];
 const cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User Flipped" + " " + cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User Flipped" + " " + cardTwo);
-console.log(cardsInPlay.length);
-if (cardsInPlay[0] === cardsInPlay[1]){
-	alert("You found a match!");
-}else{
-	alert("Sorry, try again");
+function checkForMatch() {
+	 if (cardsInPlay[0] === cardsInPlay[1]){
+		console.log("You found a match!");
+	}else{
+		console.log("Sorry, try again");
+	}
 }
+function flipCard(cardId){
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+
+}
+	
