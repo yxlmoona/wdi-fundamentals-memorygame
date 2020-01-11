fab21bd217cb694c82b9380dad186e8d99dbbc4b
Matthew Kangvanvongsa <matthewmetaveemomo@Matthews-MacBook-Pro.local>
2020-01-10 23:48:31 -0600

    create game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -32,13 +32,26 @@ function checkForMatch() {
 		alert("Sorry, try again");
 	}
 }
-function flipCard(cardId){
+function flipCard(){
+	let cardId = this.getAttribute("data-id");
+	this.setAttribute('src', cards[cardId].cardImage);
+	
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
-	
-
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+	}
 }
-	
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+createBoard();
+
